7be479ea82b22083012f91f9c22813022a7589e9
Brunnokauan <brunno_kauan@hotmail.com>
2022-02-05 22:33:21 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,33 +12,34 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::prefix('controle-series')->group(function () {
+    //Route::get('/', function () {
+    //    return view('welcome');
+    //});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/series', 'App\Http\Controllers\SeriesController@index')->name('listar_series');
+    Route::get('/series/criar', 'App\Http\Controllers\SeriesController@create')->name('form_criar_serie')/*->middleware('autenticador')*/;
+    Route::post('/series/criar', 'App\Http\Controllers\SeriesController@store')/*->middleware('autenticador')*/; 
+    Route::delete('/series/{id}', 'App\Http\Controllers\SeriesController@destroy')/*->middleware('autenticador')*/;
+    Route::post('/series/{id}/editaNome', 'App\Http\Controllers\SeriesController@editaNome')/*->middleware('autenticador')*/;
+    Route::get('/series/{serieId}/temporadas', 'App\Http\Controllers\TemporadasController@index');
+    Route::get('/temporadas/{temporada}/episodios', 'App\Http\Controllers\EpisodiosController@index');
+    Route::post('/temporadas/{temporada}/episodios/assistir', 'App\Http\Controllers\EpisodiosController@assistir')/*->middleware('autenticador')*/;
+    //Auth::routes();
 
-Route::get('/series', 'App\Http\Controllers\SeriesController@index')->name('listar_series');
-Route::get('/series/criar', 'App\Http\Controllers\SeriesController@create')->name('form_criar_serie')/*->middleware('autenticador')*/;
-Route::post('/series/criar', 'App\Http\Controllers\SeriesController@store')/*->middleware('autenticador')*/; 
-Route::delete('/series/{id}', 'App\Http\Controllers\SeriesController@destroy')/*->middleware('autenticador')*/;
-Route::post('/series/{id}/editaNome', 'App\Http\Controllers\SeriesController@editaNome')/*->middleware('autenticador')*/;
-Route::get('/series/{serieId}/temporadas', 'App\Http\Controllers\TemporadasController@index');
-Route::get('/temporadas/{temporada}/episodios', 'App\Http\Controllers\EpisodiosController@index');
-Route::post('/temporadas/{temporada}/episodios/assistir', 'App\Http\Controllers\EpisodiosController@assistir')/*->middleware('autenticador')*/;
-//Auth::routes();
+    //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    //Route::get('/entrar', 'App\Http\Controllers\EntrarController@index');
+    //Route::post('/entrar', 'App\Http\Controllers\EntrarController@entrar');
 
-//Route::get('/entrar', 'App\Http\Controllers\EntrarController@index');
-//Route::post('/entrar', 'App\Http\Controllers\EntrarController@entrar');
+    //Route::get('/registrar', 'App\Http\Controllers\RegistroController@create');
+    //Route::post('/registrar', 'App\Http\Controllers\RegistroController@store');
 
-//Route::get('/registrar', 'App\Http\Controllers\RegistroController@create');
-//Route::post('/registrar', 'App\Http\Controllers\RegistroController@store');
-
-/*Route::get('/sair', function (){
-    \Illuminate\Support\Facades\Auth::logout();
-    return redirect ('/entrar');
-});*/
+    /*Route::get('/sair', function (){
+        \Illuminate\Support\Facades\Auth::logout();
+        return redirect ('/entrar');
+    });*/
+}
 
 
 
